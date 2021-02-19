d46fd7d1ae10ebd0d8ad7091f92486bc735a396a
Hugo Soares <hugoacfs@gmail.com>
2021-02-19 11:39:46 +0000

    version bumping to 2021021900

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020120907;
+$plugin->version = 2021021900;
 $plugin->requires = 2019111800;
 $plugin->component = 'tool_coursewrangler';
 $plugin->maturity = MATURITY_ALPHA;
