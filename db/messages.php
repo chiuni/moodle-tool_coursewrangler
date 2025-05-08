07c5a9ae595ac1cf151e037c48355d7c9ca0b87e
Nigel Daley <nigel.daley@cosector.com>
2025-05-08 12:30:05 +0100

    removed deprecated message vars

--- db/messages.php
+++ db/messages.php
@@ -29,8 +29,8 @@ $messageproviders = [
     // Notify teacher that a student has submitted a quiz attempt.
     'schedulednotification' => [
         'defaults' => [
-            'popup' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN + MESSAGE_DEFAULT_LOGGEDOFF,
-            'email' => MESSAGE_PERMITTED + MESSAGE_DEFAULT_LOGGEDIN + MESSAGE_DEFAULT_LOGGEDOFF,
+            'popup' => MESSAGE_PERMITTED,
+            'email' => MESSAGE_PERMITTED,
         ],
     ]
 ];
--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022030200;
+$plugin->version = 2025050800;
 $plugin->requires = 2019111800;
 $plugin->component = 'tool_coursewrangler';
 $plugin->maturity = MATURITY_STABLE;
